d8421fb65c2f7269647ec8b70b5753d0ded0d06c
Vashu Karn <jaykarvashu@gmail.com>
2024-06-13 12:26:00 +0530

    About Section Completed

--- resources/views/web/category-detail.blade.php
+++ resources/views/web/category-detail.blade.php
@@ -1,9 +1,6 @@
 <x-guest-layout>
     @section('page_title', $page_title)
-    @php
-    $background_image = asset('background.png');
-    @endphp
-    <section id="mt_banner_border" style="background-image: url('{{ $background_image }}')">
+    <section id="mt_banner_border" style="background-image: url('{{ $category->banner }}')">
         <div class="banner-wrapper">
             <div class="banner-caption">
                 <h2 style="color: #fff;" class="wow fadeInUp" data-wow-delay="0.1">{{ $category->title }}</h2>
